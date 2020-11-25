d6cac954e3eb6b5f89c321e95bdd53c698c7652b
Aleksandr Khorunzhev <sanjiksanjiksanjik@gmail.com>
2020-11-25 21:52:11 +0400

    initial commit

---
+++ src/main/java/ru/otus/spring/homework1/Main.java
@@ -0,0 +1,13 @@
+package ru.otus.spring.homework1;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Main {
+
+    public static void main(String[] args) {
+        ClassPathXmlApplicationContext context =
+                new ClassPathXmlApplicationContext("/spring-context.xml");
+
+
+    }
+}
