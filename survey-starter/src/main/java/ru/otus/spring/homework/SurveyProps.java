1c4a3e51a75b2a3994c22ea47abecca20305c23b
Александр Хорунжев <horunzhev@tecforce.ru>
2020-12-11 13:04:35 +0400

    PR fixes

--- survey-starter/src/main/java/ru/otus/spring/homework/SurveyProps.java
+++ survey-starter/src/main/java/ru/otus/spring/homework/SurveyProps.java
@@ -9,54 +9,21 @@ import java.util.Locale;
 @Data
 public class SurveyProps {
 
-    private int numberOfQuestions;
-    private int numberOfRightAnswers;
-    private Locale locale;
-    private String fileName;
-
-
-    public int getNumberOfQuestions() {
-        if (numberOfQuestions != 0) {
-            return numberOfQuestions;
-        } else {
-            return getDefaultNumberOfQuestions();
-        }
-    }
+    private int numberOfQuestions = getDefaultNumberOfQuestions();
+    private int numberOfRightAnswers = getDefaultNumberOfRightAnswers();
+    private Locale locale = getDefaultLocale();
+    private String fileName = getDefaultFile();
 
     private int getDefaultNumberOfQuestions() {
         return 5;
-    }
-
-    public int getNumberOfRightAnswers() {
-        if (numberOfRightAnswers != 0) {
-            return numberOfRightAnswers;
-        } else {
-            return getDefaultNumberOfRightAnswers();
-        }
     }
 
     private int getDefaultNumberOfRightAnswers() {
         return 1;
     }
 
-    public Locale getLocale() {
-        if (locale != null) {
-            return locale;
-        } else {
-            return getDefaultLocale();
-        }
-    }
-
     private Locale getDefaultLocale() {
         return Locale.ENGLISH;
-    }
-
-    public String getFileName() {
-        if (fileName != null) {
-            return fileName;
-        } else {
-            return getDefaultFile();
-        }
     }
 
     private String getDefaultFile() {
